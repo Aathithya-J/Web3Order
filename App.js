442f391145f10bd152ddf2d21abd65d4fafcc662
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2023-05-15 13:43:09 +0800

    Coffe Shop Update

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
+    <SafeAreaView style={{backgroundColor: '#DDB982'}}>
     <View>
-      <Text style={{color: "red", textAlign: 'center', paddingTop: 100, fontSize:20}}>Welcome to MathX!</Text>
+      <Text style={{
+        color: "black", 
+        textAlign: 'center', 
+        paddingTop: 100, 
+        fontSize:40, 
+        fontWeight:'bold'
+        }}>your perfect Brew!
+      </Text>  
+      <Text style={{
+        color: "black", 
+        textAlign: 'center', 
+        paddingBottom:10, 
+        fontSize:20, 
+        }}>lorem ipusm!
+      </Text>  
       <StatusBar style="auto"/>
+      <Button title="sign in" color="#841584"/>
+      <Button title="dont have an account?" color="#841584"/>
+      <Button title="create account" color="#841584"/>
     </View>
+  </SafeAreaView>
   );
 }
